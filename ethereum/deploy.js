0c4ce341a7c511d588d18b4599d0db969d22eb74
bharathraj25 <86262066+bharathraj25@users.noreply.github.com>
2021-08-31 14:07:38 +0530

    Updated with new Provider

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -3,10 +3,9 @@ const Web3 = require('web3');
 const compiledFactory = require('./build/CampaignFactory.json');
 
 const provider = new HDWalletProvider(
-  'vibrant fly dove rebuild castle trade imitate torch tray trial first option',
-  'https://rinkeby.infura.io/v3/15a4a56923b14777a2437b58f1c59484'
+  'betray resource option off toss fitness festival dune broom section adult swift',
+  'https://rinkeby.infura.io/v3/d7c7233f0f55411d9207fb334cdfdeeb'
 );
-
 const web3 = new Web3(provider);
 
 const deploy = async () => {
@@ -14,13 +13,12 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
     .deploy({ data: compiledFactory.bytecode })
-    .send({ gas: '5000000', from: accounts[0] });
+    .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
 };
 deploy();
-
-
-//0xA24b0bd3956bD29F1C8339d344fe9b608F65248e
