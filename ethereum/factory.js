00f8eb7be50c9c56a0218e577761df684ca09ae4
bharathraj25 <86262066+bharathraj25@users.noreply.github.com>
2021-08-31 14:05:23 +0530

    updated factory-address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,9 +1,9 @@
 import web3 from './web3';
-import CampaginFactory from './build/CampaignFactory.json';
+import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
-  JSON.parse(CampaginFactory.interface),
-  '0xA24b0bd3956bD29F1C8339d344fe9b608F65248e'
+  JSON.parse(CampaignFactory.interface),
+  '0x40dde621d953C6B12d474cB86004eA56E12a15F8'
 );
 
 export default instance;
