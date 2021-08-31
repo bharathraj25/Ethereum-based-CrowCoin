63855d9abb5d69256a7c5c40228283c8b04332c7
bharathraj25 <86262066+bharathraj25@users.noreply.github.com>
2021-08-31 14:00:59 +0530

    Update web3.js

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -2,13 +2,13 @@ import Web3 from 'web3';
 
 let web3;
 
-if(typeof window !== 'undefined' && window.web3 !== 'undefined'){
-  // We are on a browser and metamask is running
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+  // We are in the browser and metamask is running.
   web3 = new Web3(window.web3.currentProvider);
-}else{
-  // We are on the Server OR the user isn't runnig metamask
+} else {
+  // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
-    'https://rinkeby.infura.io/v3/15a4a56923b14777a2437b58f1c59484'
+    'https://rinkeby.infura.io/v3/d7c7233f0f55411d9207fb334cdfdeeb'
   );
   web3 = new Web3(provider);
 }
